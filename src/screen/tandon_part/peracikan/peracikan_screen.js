0546d2c13b3322b07cc72734aeef2eb93d8024d7
azrinaldi <dickoazri791@gmail.com>
2023-12-11 11:50:04 +0700

    Update

--- src/screen/tandon_part/peracikan/peracikan_screen.js
+++ src/screen/tandon_part/peracikan/peracikan_screen.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {View, Text, TextInput, Button, TouchableOpacity} from 'react-native';
-import {Dropdown} from 'react-native-element-dropdown';
+import Dropdown from 'react-native-select-dropdown';
 import Icon from 'react-native-vector-icons/Ionicons';
 import axios from 'axios';
 
